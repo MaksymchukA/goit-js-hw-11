bbf0f95c7af439fbd285aed4f53c62ab1c780f1b
MaksymchukA <nastena_1991@ukr.net>
2023-02-10 01:44:29 +0200

    did the eleventh task

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -3,29 +3,21 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '33456523-920060555a2604cf0d60222c4';
 
-// export function getImages(query) {
-//   const response = axios.get(
-//     `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
-//   );
-//   console.log('🚀 ~ getImages ~ response', response);
-
-//   return response.then(response => response.data);
-// }
-
 export default class ImageApi {
   constructor() {
     this.searchQuery = '';
     this.page = 1;
   }
-  getImages() {
-    const response = axios.get(
+
+  async getImages() {
+    const response = await axios.get(
       `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
     );
-    // console.log('🚀 ~ getImages ~ response', response);
 
     this.incrementPage();
 
-    return response.then(response => response.data);
+    // return response.then(response => response.data);
+    return response.data;
   }
 
   incrementPage() {
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,28 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import { getImages } from './api';
 import ImageApi from './api';
 import LoadMoreBtn from './load-more-btn';
 
 const searchForm = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
+const text = document.querySelector('.end-collection');
 const imageApi = new ImageApi();
 const loadMoreBtn = new LoadMoreBtn({
   selector: '.load-more',
   isHidden: true,
 });
-imageApi.searchQuery = '';
 
 searchForm.addEventListener('submit', onFormSubmit);
 loadMoreBtn.button.addEventListener('click', fetchImages);
 
 function onFormSubmit(event) {
   event.preventDefault();
-  console.log(event);
+  // console.log(event);
 
   imageApi.searchQuery = event.currentTarget.elements.searchQuery.value.trim();
+
+  if (!imageApi.searchQuery) {
+    return;
+  }
 
   imageApi.resetPage();
   clearGallery();
@@ -29,57 +32,79 @@ function onFormSubmit(event) {
   searchForm.reset();
 }
 
-function fetchImages() {
-  return imageApi.getImages().then(data => {
-    console.log(data);
-    searchImages(data);
+async function fetchImages() {
+  loadMoreBtn.hide();
+  text.classList.add('hidden');
 
-    // Notify.success(`Hooray! We found ${data.totalHits} images.`);
-  });
+  const data = await imageApi.getImages();
+  console.log(data);
+  searchImages(data);
+  return data;
 }
 
 function searchImages(imagesArray) {
-  loadMoreBtn.hide();
-  if (imagesArray.hits.length === 0 || imageApi.searchQuery === '') {
-    return Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
+  const currentPage = imageApi.page - 1;
+
+  if (imagesArray.hits.length > 40) {
+    loadMoreBtn.show();
+  } else {
+    loadMoreBtn.hide();
   }
 
-  renderedMarkupImages(imagesArray);
-  loadMoreBtn.show();
-  // updateImagesGallery();
+  if (imagesArray.hits.length === 0) {
+    loadMoreBtn.hide();
+    text.classList.add('hidden');
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+
+  if (imagesArray.hits.length > 0) {
+    renderedMarkupImages(imagesArray);
+    loadMoreBtn.show();
+    text.classList.add('hidden');
+  }
+
+  if (imagesArray.hits.length < 40) {
+    loadMoreBtn.hide();
+    text.classList.remove('hidden');
+  }
+
+  if (currentPage === 1) {
+    Notify.success(`Hooray! We found ${imagesArray.totalHits} images.`);
+  }
 }
 
 function renderedMarkupImages({ hits }) {
-  const markup = hits.map(hit => {
-    return `<div class="photo-card">
-  <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
-  <div class="info">
+  const markup = hits
+    .map(hit => {
+      return `
+    <div class="photo-card">
+    <a href="${hit.largeImageURL}">
+    <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
+    </a>
+    <div class="info">
     <p class="info-item">
-      <b>Likes: ${hit.likes}</b>
+      <b>Likes ${hit.likes}</b>
     </p>
     <p class="info-item">
-      <b>Views: ${hit.views}</b>
+      <b>Views ${hit.views}</b>
     </p>
     <p class="info-item">
-      <b>Comments: ${hit.comments}</b>
+      <b>Comments ${hit.comments}</b>
     </p>
     <p class="info-item">
-      <b>Downloads: ${hit.downloads}</b>
+      <b>Downloads ${hit.downloads}</b>
     </p>
-  </div>
-</div>`;
-  });
-  gallery.innerHTML = markup;
+    </div>
+    </div>
+    `;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
 
 function clearGallery() {
   gallery.innerHTML = '';
 }
-
-// function updateImagesGallery() {
-//   gallery.insertAdjacentHTML('beforeend', );
-// }
-
-function onError(error) {}
